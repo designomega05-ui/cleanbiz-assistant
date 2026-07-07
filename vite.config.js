4c43435c5ebd059cc46421e52d5a3132465b68e2
designomega05-ui <designomega05@gmail.com>
2026-07-06 23:40:35 -0500

    Add Tailwind CSS plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    lightningcss: false,
-  },
-  build: {
-    cssMinify: false,
-  },
+  plugins: [react(), tailwindcss()],
 })
